0cfacd815b4d6b6054d7daf8a5fab0fa3f32f203
Gints <gintskristaps13@gmail.com>
2025-10-05 22:18:06 +0300

    config fix

--- install/post/zsh.sh
+++ install/post/zsh.sh
@@ -2,5 +2,5 @@
 THIS_SCRIPT_DIR=$(dirname "$(realpath "$0")")
 
 
-cp -r $THIS_SCRIPT_DIR/../../default/zsh/zsh $HOME/.config/zsh
+cp -r $THIS_SCRIPT_DIR/../../default/zsh/zsh/* $HOME/.config/zsh
 cp $THIS_SCRIPT_DIR/../../default/zsh/.zshrc $HOME/.zshrc
