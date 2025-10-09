ff363e054a028a8b81a0093bf825ac9cde8ccb2b
Gints <gintskristaps13@gmail.com>
2025-10-10 00:21:39 +0300

    audio setup

--- install/post/startup.sh
+++ install/post/startup.sh
@@ -3,4 +3,5 @@
 sudo systemctl enable --now NetworkManager
 #bluetooth
 sudo systemctl enable --now bluetooth.service
-
+#audio
+systemctl --user enable --now pipewire.service pipewire.socket wireplumber.service pipewire-pulse.service 
